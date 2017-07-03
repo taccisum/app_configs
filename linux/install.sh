c7fc746d01a4ba40663563636f4d538d997e8ceb
tac <tac@localhost.localdomain>
2017-07-04 01:03:37 +0800

    optimize install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,21 @@
-if [ $(whoami) == "root" ]; then
-	yum install vim
-	yum install tree
-	yum install git
-else
+function install(){
+	echo "installing" $1
+	yum install $1 -q $2
+}
+
+confirm="-y"
+
+if [ $(whoami) != "root" ]; then
 	echo "请切换为root用户执行"
+	exit 1;
 fi
 
+read -p "ask you when ready to install? y/n: " yn
+if [ "$yn" == "y" ] || [ "$yn" == "Y" ]; then
+	confirm=""
+fi
 
+install vim $confirm
+install tree $confirm
+install git $confirm
+install java-1.8.0-openjdk $confirm
